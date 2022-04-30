22d629f649e7007a873ba775cda8988bd6809e70
JON9HUN <whdgns4195@gmail.com>
2022-04-30 20:40:11 +0900

    almost finish

--- Lighting your Selfie/main.js
+++ Lighting your Selfie/main.js
@@ -1,12 +1,9 @@
-const videoElement = document.getElementsByClassName("input_video")[0];
+const videoElement = document.getElementsByClassName("input_webcam")[0];
 const canvasElement = document.getElementsByClassName("output_canvas")[0];
 const canvasCtx = canvasElement.getContext("2d");
 
 document.addEventListener("mousedown", MouseDown, false);
-document.addEventListener("mousemove", MouseMove, false);
 document.addEventListener("mouseup", MouseUp, false);
-
-// document.addEventListener("mousewheel", MouseWheel, false);
 
 import * as THREE from "three";
 import { TRIANGULATION } from "./triangulation.js";
@@ -24,7 +21,6 @@ document.body.appendChild(renderer.domElement);
 
 const camera_ar = new THREE.PerspectiveCamera(60, r_height / r_width, 50, 300);
 camera_ar.position.set(0, 0, 100);
-// camera_ar.lookAt(0, 0, 0);
 camera_ar.up.set(0, 1, 0);
 const camera_helper = new THREE.CameraHelper(camera_ar);
 
@@ -37,47 +33,35 @@ const camera_world = new THREE.PerspectiveCamera(
   45,
   r_height / r_width,
   1,
-  500
+  2000
 );
 camera_world.position.set(50, 50, 150);
-// camera_ar.lookAt(0, 0, 0);
 camera_world.up.set(0, 1, 0);
 
-const light = new THREE.DirectionalLight(0xffffff, 1.0);
+const light = new THREE.DirectionalLight(0xffffff, 1.2);
 light.position.set(0, 0, camera_ar.position.z - camera_ar.near);
-// light.position.set(50, 0, 99);
-const ambientlight = new THREE.AmbientLight(0xffffff, 0.4);
+const ambientlight = new THREE.AmbientLight(0xffffff, 0.2);
 ambientlight.position.set(0, 0, camera_ar.near);
+const light_helper = new THREE.DirectionalLightHelper(light, 0);
 
-const light_helper = new THREE.DirectionalLightHelper(light, 1);
-console.log("light_position", light.position);
 const controls = new OrbitControls(camera_world, renderer_world.domElement);
-controls.update();
-
-let mouseX = 0,
-  mouseY = 0;
-let HalfX = r_width / 2;
-let HalfY = r_height / 2;
+controls.enableDamping = true;
+controls.dampingFactor = 0.1;
 
 const mouse = new THREE.Vector2();
 const rayCast = new THREE.Raycaster();
 
 const scene = new THREE.Scene();
-const texture_bg = new THREE.VideoTexture(videoElement);
-scene.background = texture_bg;
+const video_texture = new THREE.VideoTexture(videoElement);
 
 const frustumHeight =
   2.0 * camera_ar.far * Math.tan(THREE.MathUtils.degToRad(camera_ar.fov * 0.5));
-console.log("h", frustumHeight);
 const frustumWidth = frustumHeight * camera_ar.aspect;
 
 const plane_geo = new THREE.PlaneGeometry(frustumWidth, frustumHeight);
-// plane_geo.scale(0.2, 0.2, 0.2);
-const plane_mat = new THREE.MeshBasicMaterial({ map: texture_bg });
+const plane_mat = new THREE.MeshBasicMaterial({ map: video_texture });
 const video_mesh = new THREE.Mesh(plane_geo, plane_mat);
 video_mesh.position.set(0, 0, camera_ar.position.z - camera_ar.far);
-
-console.log(camera_ar.position.z - camera_ar.far);
 
 scene.add(light);
 scene.add(ambientlight);
@@ -87,7 +71,7 @@ scene.add(video_mesh);
 
 let oval_point_mesh = null;
 let oval_line = null;
-let face_mesh = null; // value for THREE
+let face_mesh = null;
 
 function ProjScale(p_ms, cam_pos, src_d, dst_d) {
   let vec_cam2p = new THREE.Vector3().subVectors(p_ms, cam_pos);
@@ -141,11 +125,10 @@ function onResults2(results) {
         let oval_point_geo = new THREE.BufferGeometry();
         let oval_line_geo = new LineGeometry();
         const num_oval_points = FACEMESH_FACE_OVAL.length;
-        const oval_vertices = []; //new Float32Array(num_oval_points);
+        const oval_vertices = [];
         for (let i = 0; i < num_oval_points; i++) {
           const index = FACEMESH_FACE_OVAL[i][0];
           const pos_ns = landmarks[index];
-          // 0 ~ 1 -> -1 ~ 1
           const pos_ps = new THREE.Vector3(
             (pos_ns.x - 0.5) * 2,
             -(pos_ns.y - 0.5) * 2,
@@ -156,9 +139,7 @@ function onResults2(results) {
             pos_ps.y,
             pos_ps.z
           ).unproject(camera_ar);
-          //oval_vertices[i] = pos_ws;
           oval_vertices.push(pos_ws.x, pos_ws.y, pos_ws.z);
-          // oval_vertices_line.push(pos_ws.x, pos_ws.y, pos_ws.z);
         }
         const oval_vertices_line = oval_vertices.slice();
         oval_vertices_line.push(
@@ -166,26 +147,22 @@ function onResults2(results) {
           oval_vertices_line[1],
           oval_vertices_line[2]
         );
-        // console.log(oval_vertices);
-        // oval_vertices_line = oval_vertices;
-        // oval_vertices_line.push(oval_vertices[0]);
+
         const point_mat = new THREE.PointsMaterial({
           color: 0xff0000,
           size: 3,
         });
+
         const line_mat = new LineMaterial({
           color: 0x00ff00,
-          linewidth: 0.01, // in world units with size attenuation, pixels otherwise
-          // vertexColors: true,
-          //resolution:  // to be set by renderer, eventually
-          dashed: false,
-          //   alphaToCoverage: true,
+          linewidth: 0.01,
         });
-        // oval_point_geo.setFromPoints(oval_vertices);
+
         oval_point_geo.setAttribute(
           "position",
           new THREE.Float32BufferAttribute(oval_vertices, 3)
         );
+
         oval_line_geo.setPositions(oval_vertices_line);
         oval_point_mesh = new THREE.Points(oval_point_geo, point_mat);
         oval_line = new Line2(oval_line_geo, line_mat);
@@ -226,12 +203,11 @@ function onResults2(results) {
 
       const num_oval_points = FACEMESH_FACE_OVAL.length;
       let positions = oval_point_mesh.geometry.attributes.position.array;
-      let positions_line = new Float32Array(positions.length + 9);
+      let positions_line = new Float32Array(positions.length + 3);
 
       for (let i = 0; i < num_oval_points; i++) {
         const index = FACEMESH_FACE_OVAL[i][0];
         const pos_ns = landmarks[index];
-        // 0 ~ 1 -> -1 ~ 1
         const pos_ps = new THREE.Vector3(
           (pos_ns.x - 0.5) * 2,
           -(pos_ns.y - 0.5) * 2,
@@ -240,7 +216,6 @@ function onResults2(results) {
         let pos_ws = new THREE.Vector3(pos_ps.x, pos_ps.y, pos_ps.z).unproject(
           camera_ar
         );
-        //oval_vertices[i] = pos_ws;
         pos_ws = ProjScale(pos_ws, camera_ar.position, center_dist, 100.0);
         positions[3 * i + 0] = pos_ws.x;
         positions[3 * i + 1] = pos_ws.y;
@@ -253,26 +228,14 @@ function onResults2(results) {
           positions_line[3 * i + 3] = positions_line[0];
           positions_line[3 * i + 4] = positions_line[1];
           positions_line[3 * i + 5] = positions_line[2];
-
-          positions_line[3 * i + 6] = positions_line[3];
-          positions_line[3 * i + 7] = positions_line[4];
-          positions_line[3 * i + 8] = positions_line[5];
-
-          positions_line[3 * i + 9] = positions_line[6];
-          positions_line[3 * i + 10] = positions_line[7];
-          positions_line[3 * i + 11] = positions_line[8];
         }
       }
       oval_point_mesh.geometry.attributes.position.needsUpdate = true;
       oval_line.geometry.setPositions(positions_line);
-      // oval_line.geometry.attributes.position.needsUpdate = positions;
-      // oval_line.geometry.attributes.position.needsUpdate = true;
 
-      const num_points = landmarks.length; // type script 에서는.. compiled language 에선 필요 X 48:45
+      const num_points = landmarks.length;
       for (let i = 0; i < num_points; i++) {
-        // const index = landmarks[i];
         const pos_ns = landmarks[i];
-        // 0 ~ 1 -> -1 ~ 1
         const pos_ps = new THREE.Vector3(
           (pos_ns.x - 0.5) * 2,
           -(pos_ns.y - 0.5) * 2,
@@ -282,8 +245,7 @@ function onResults2(results) {
           camera_ar
         );
         pos_ws = ProjScale(pos_ws, camera_ar.position, center_dist, 100.0);
-        // console.log(pos_ws);
-        //oval_vertices[i] = pos_ws;
+
         face_mesh.geometry.attributes.position.array[3 * i + 0] = pos_ws.x;
         face_mesh.geometry.attributes.position.array[3 * i + 1] = pos_ws.y;
         face_mesh.geometry.attributes.position.array[3 * i + 2] = pos_ws.z;
@@ -300,8 +262,15 @@ function onResults2(results) {
       light.target = face_mesh;
     }
   }
+  scene.remove(light_helper);
+  scene.remove(camera_helper);
   renderer.render(scene, camera_ar);
+
+  scene.add(light_helper);
+  scene.add(camera_helper);
   renderer_world.render(scene, camera_world);
+  controls.update();
+
   canvasCtx.restore();
 }
 
@@ -310,6 +279,7 @@ const faceMesh = new FaceMesh({
     return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
   },
 });
+
 faceMesh.setOptions({
   maxNumFaces: 1,
   refineLandmarks: true,
@@ -318,14 +288,15 @@ faceMesh.setOptions({
 });
 faceMesh.onResults(onResults2);
 
-/*const camera = new Camera(videoElement, {
+const camera = new Camera(videoElement, {
   onFrame: async () => {
     await faceMesh.send({ image: videoElement });
   },
-  width: 1280,
-  height: 720,
-});*/
-// camera.start();
+  width: r_width,
+  height: r_height,
+});
+camera.start();
+
 let IsPressed = false;
 function MouseDown(e) {
   IsPressed = true;
@@ -334,46 +305,47 @@ function MouseUp(e) {
   IsPressed = false;
 }
 
-function MouseMove(e) {
-  if (IsPressed == true) {
-    mouse.x = (e.clientX / r_width) * 2 - 1;
-    mouse.y = -(e.clientY / r_height) * 2 + 1;
+renderer.domElement.addEventListener(
+  "mousemove",
+  function (e) {
+    if (IsPressed == true) {
+      mouse.x = (e.clientX / r_width) * 2 - 1;
+      mouse.y = -(e.clientY / r_height) * 2 + 1;
 
-    rayCast.setFromCamera(mouse, camera_ar);
+      rayCast.setFromCamera(mouse, camera_ar);
+      let n;
+      n = -camera_ar.near / rayCast.ray.direction.z;
 
-    // let intersects = rayCast.intersectObjects(scene.children);
-    // console.log(intersects[0]);
-    // if (intersects.length == 0) {
-    //   return;
-    // }
-    // console.log(rayCast.ray);
-    let n;
-    n = -camera_ar.near / rayCast.ray.direction.z;
-    // console.log(n);
+      light.position.set(
+        rayCast.ray.direction.x * n,
+        rayCast.ray.direction.y * n,
+        camera_ar.position.z + rayCast.ray.direction.z * n
+      );
+      light.lookAt(0, 0, 0);
+    }
+  },
+  false
+);
 
-    light.position.set(
-      rayCast.ray.direction.x * n,
-      rayCast.ray.direction.y * n,
-      camera_ar.position.z + rayCast.ray.direction.z * n
-    );
-
-    console.log(light.position);
-    light.target = face_mesh;
-    // if (INTERSECTED != intersects[0].object) {
-    //   INTERSECTED = intersects[0].object;
-    //   scene.remove(INTERSECTED);
-    //   audio.play();
-    //   //console.log(INTERSECTED);
-    // }
-  }
-  // MouseMove(e);
-}
-
-videoElement.muted = true;
-videoElement.play();
-
-async function detectFrame(now, metadata) {
-  await faceMesh.send({ image: videoElement });
-  videoElement.requestVideoFrameCallback(detectFrame);
-}
-detectFrame();
+const wheeltodistance = 2;
+let near_og;
+renderer.domElement.addEventListener(
+  "mousewheel",
+  function (e) {
+    near_og = camera_helper.camera.near;
+    if (e.wheelDelta < 0) {
+      camera_helper.camera.near -= wheeltodistance;
+      light.position.z += wheeltodistance;
+    } else if (e.wheelDelta > 0) {
+      light.position.z -= wheeltodistance;
+      camera_ar.near += wheeltodistance;
+    }
+    light.position.x *= camera_helper.camera.near / near_og;
+    light.position.y *= camera_helper.camera.near / near_og;
+    light.lookAt(0, 0, 0);
+    camera_ar.updateProjectionMatrix();
+    camera_helper.update();
+    light_helper.update();
+  },
+  false
+);
